c220e970d9b0814d92819977ee9ee8e30384659e
cnrustic <cnrustic@live.cn>
2024-11-25 10:28:33 +0800

    Update template-functions.php

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -2,36 +2,53 @@
 /**
  * Functions which enhance the theme by hooking into WordPress
  *
- * @package architizer
+ * @package Architizer
  */
 
-/**
- * Adds custom classes to the array of body classes.
- *
- * @param array $classes Classes for the body element.
- * @return array
- */
-function architizer_body_classes( $classes ) {
-	// Adds a class of hfeed to non-singular pages.
-	if ( ! is_singular() ) {
-		$classes[] = 'hfeed';
-	}
+if (!function_exists('architizer_body_classes')) {
+    /**
+     * Adds custom classes to the array of body classes.
+     *
+     * @param array $classes Classes for the body element.
+     * @return array
+     */
+    function architizer_body_classes($classes) {
+        // Adds a class of hfeed to non-singular pages.
+        if (!is_singular()) {
+            $classes[] = 'hfeed';
+        }
 
-	// Adds a class of no-sidebar when there is no sidebar present.
-	if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-		$classes[] = 'no-sidebar';
-	}
+        // Adds a class of no-sidebar when there is no sidebar present.
+        if (!is_active_sidebar('sidebar-1')) {
+            $classes[] = 'no-sidebar';
+        }
 
-	return $classes;
+        return $classes;
+    }
 }
-add_filter( 'body_class', 'architizer_body_classes' );
 
-/**
- * Add a pingback url auto-discovery header for single posts, pages, or attachments.
- */
-function architizer_pingback_header() {
-	if ( is_singular() && pings_open() ) {
-		printf( '<link rel="pingback" href="%s">', esc_url( get_bloginfo( 'pingback_url' ) ) );
-	}
+if (!function_exists('count_firm_projects')) {
+    /**
+     * 统计建筑事务所的项目数量
+     *
+     * @param int $firm_id 建筑事务所ID
+     * @return int 项目数量
+     */
+    function count_firm_projects($firm_id) {
+        $args = array(
+            'post_type' => 'project',
+            'meta_query' => array(
+                array(
+                    'key' => 'project_firm',
+                    'value' => $firm_id,
+                    'compare' => '='
+                )
+            ),
+            'posts_per_page' => -1
+        );
+        $query = new WP_Query($args);
+        return $query->found_posts;
+    }
 }
-add_action( 'wp_head', 'architizer_pingback_header' );
+
+// 其他自定义函数...
